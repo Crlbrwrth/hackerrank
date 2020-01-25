9da5d158795408962f2e270ae8bd72bf95276770
Devin Fiachra <devinduffymeade@gmail.com>
2020-01-25 12:55:59 +0100

    problem solved wihout timeout

--- problem_solving/sherlok_and_squares/devin.js
+++ problem_solving/sherlok_and_squares/devin.js
@@ -1,10 +1,31 @@
+// TOO SLOW!
+
+// function squares(a, b) {
+//     let arr = [];
+//     for (let i = a; i <= b; i++) {
+//         if (Math.sqrt(i) % 1 === 0) {
+//             arr.push(i);
+//         }
+//     }
+//
+//     return arr.length;
+// }
+//
+// console.log(squares(1, 10000000));
+
 function squares(a, b) {
-    let arr = [];
-    for (let i = a; i <= b; i++) {
-        if (Math.sqrt(i) % 1 === 0) {
-            arr.push(i);
+    let currentSquare = 0,
+        i = 0,
+        perfectSquares = 0;
+
+    while (currentSquare < b) {
+        currentSquare = i * i;
+
+        if (currentSquare >= a && currentSquare <= b) {
+            perfectSquares++;
         }
+        i++;
     }
 
-    return arr.length;
+    return perfectSquares;
 }
